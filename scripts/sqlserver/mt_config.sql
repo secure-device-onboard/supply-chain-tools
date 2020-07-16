6b0d089ea048a5610b902cfc99af7742d3d2a619
Easterday, John <john.easterday@intel.com>
2020-07-15 17:03:42 -0700

    add instruction for multiple rendezvous info entries
    Signed-off-by: Easterday, John <john.easterday@intel.com>

--- scripts/sqlserver/mt_config.sql
+++ scripts/sqlserver/mt_config.sql
@@ -6,7 +6,9 @@ GO
 
 --call mt_add_server_settings(rendezvous info, certificate_validity_period);
 --rendezvous info = specifies the rendezvous server the device will contact during
---SDO TO protocol (initial boot at end customer site
+--SDO TO protocol (initial boot at end customer site)
+--Multiple rendezvous entries can be specified by a space delimited list. For example:
+--@rendezvous_info = 'http://sdo-sbx.trustedservices.intel.com:80 http://sdo-pbx.trustedservices.intel.com:80',
 --certificate_validity_period = period for ECC based device cert chain
 --certificate_validity_period is specified according to ISO 8601 duration value
 --duration format is: PnYnMnDTnHnMnS
